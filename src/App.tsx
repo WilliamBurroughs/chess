4759d8c2f960eebb56f74fd624d677767d1e2e28
William Burroughs <berroughswilliam@gmail.com>
2023-02-12 00:13:56 +0300

    board creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import BoardComponent from "./components/BoardCompotent";
+import CellComponent from "./components/CellComponent";
+import { Board } from "./models/Board";
 
 const App = () => {
-  return <div></div>;
+  const [board, setBoard] = useState(new Board()); // [state, setState] = useState(initialState)  return значение состояния и функция для его обновления
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+  return (
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard} />
+    </div>
+  );
 };
 
 export default App;
